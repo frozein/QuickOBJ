cbc26af55355ac57ec5ff13d9fd7759f2ea0161f
frozein <elwell.daniel.2@gmail.com>
2023-04-01 20:30:04 -0400

    finished v, vn, vt commands. started usemtl, f

--- src/quickobj.h
+++ src/quickobj.h
@@ -26,6 +26,12 @@ typedef struct QOBJvec3
 	float v[2];
 } QOBJvec3;
 
+//a 3-dimensional vector of unsigned integers
+typedef struct QOBJuvec3
+{
+	uint32_t v[3];
+} QOBJuvec3;
+
 //a single vertex with position, normal, and texture coordinates
 typedef struct QOBJvertex
 {
@@ -49,6 +55,7 @@ typedef struct QOBJmesh
 	size_t numIndices;
 	uint32_t* indices;
 
+	const char* materialName;
 	QOBJmaterial material;
 } QOBJmesh;
 
@@ -57,11 +64,64 @@ typedef enum QOBJerror
 {
 	QOBJ_SUCCESS = 0,
 	QOBJ_ERROR_INVALID_FILE,
-	QOBJ_ERROR_IO
+	QOBJ_ERROR_IO,
+	QOBJ_ERROR_MAX_TOKEN_LEN,
+	QOBJ_ERROR_OUT_OF_MEM,
+	QOBJ_ERROR_UNSUPPORTED_DATA_TYPE
 } QOBJerror;
 
 //----------------------------------------------------------------------//
 //FUNCTIONS:
+
+static inline QOBJerror qobj_next_token(FILE* fptr, size_t maxTokenLen, char** token, char* endCh)
+{
+	char curCh;
+	size_t curLen = 0;
+
+	while(true)
+	{
+		if(curLen >= maxTokenLen)
+			return QOBJ_ERROR_MAX_TOKEN_LEN;
+
+		curCh = fgetc(fptr);
+
+		if(curCh == ' ' || curCh == '\n' || curCh == EOF)
+			break;
+
+		(*token)[curLen++] = curCh;
+	}
+
+	(*token)[curLen] = '\0';
+	*endCh = curCh;
+
+	return QOBJ_SUCCESS;
+}
+
+static inline QOBJerror qobj_maybe_resize_buffer(void** buffer, size_t elemSize, size_t numElems, size_t* elemCap)
+{
+	if(numElems < *elemCap)
+		return QOBJ_SUCCESS;
+	
+	*elemCap *= 2;
+	*buffer = realloc(*buffer, *elemCap * elemSize);
+	if(!*buffer)
+		return QOBJ_ERROR_OUT_OF_MEM;
+
+	return QOBJ_SUCCESS;
+}
+
+static void qobj_free(size_t numMeshes, QOBJmesh* meshes)
+{
+	for(int i = 0; i < numMeshes; i++)
+	{
+		free(meshes[i].vertices);
+		free(meshes[i].indices);
+		free(meshes[i].materialName)
+	}
+
+	if(numMeshes > 0)
+		free(meshes);
+}
 
 static QOBJerror qobj_load(const char* path, size_t* numMeshes, QOBJmesh** meshes)
 {
@@ -77,15 +137,151 @@ static QOBJerror qobj_load(const char* path, size_t* numMeshes, QOBJmesh** meshe
 	QOBJerror errorCode = QOBJ_SUCCESS;
 
 	//initialize memory:
-	size_t positionSize, normalSize, texCoordSize = 32;
-	QOBJvec3* positions = malloc(positionSize * sizeof(QOBJvec3));
-	QOBJvec3* normals   = malloc(normalSize   * sizeof(QOBJvec3));
-	QOBJvec2* texCoords = malloc(texCoordSize * sizeof(QOBJvec2));
+	size_t positionSize, normalSize, texCoordSize = 0;
+	size_t positionCap, normalCap, texCoordCap = 32;
+	QOBJvec3* positions = malloc(positionCap * sizeof(QOBJvec3));
+	QOBJvec3* normals   = malloc(normalCap   * sizeof(QOBJvec3));
+	QOBJvec2* texCoords = malloc(texCoordCap * sizeof(QOBJvec2));
 
-	*numMeshes = 1;
-	*meshes = malloc(1 * sizeof(QOBJmesh));
+	*numMeshes = 0;
+
+	const size_t MAX_TOKEN_LEN = 64;
+	char* curToken = malloc((MAX_TOKEN_LEN + 1) * sizeof(char));
+	char curTokenEnd;
+
+	size_t curMesh = 1;
+
+	while(true)
+	{
+		QOBJerror tokenError = qobj_next_token(fptr, MAX_TOKEN_LEN, &curToken, &curTokenEnd);
+		if(tokenError != QOBJ_SUCCESS)
+		{
+			errorCode = tokenError;
+			goto cleanup;
+		}
+
+		if(curToken[0] == '#' || strcmp(curToken, "o") == 0 || strcmp(curToken, "g") == 0) //comment / ignored commands
+		{
+			if(curTokenEnd == ' ')
+				fgets(curToken, MAX_TOKEN_LEN, fptr);
+		}
+		else if(strcmp(curToken, "v") == 0)
+		{
+			QOBJvec3 pos;
+			fscanf(fptr, "%f %f %f\n", &pos.v[0], &pos.v[1], &pos.v[2]);
+
+			positions[positionSize++] = pos;
+			QOBJerror resizeError = qobj_maybe_resize_buffer(&positions, sizeof(QOBJvec3), positionSize, &positionCap);
+			if(resizeError != QOBJ_SUCCESS)
+			{
+				errorCode = resizeError;
+				goto cleanup;
+			}
+		}
+		else if(strcmp(curToken, "vn") == 0)
+		{
+			QOBJvec3 normal;
+			fscanf(fptr, "%f %f %f\n", &normal.v[0], &normal.v[1], &normal.v[2]);
+
+			normals[normalSize++] = normal;
+			QOBJerror resizeError = qobj_maybe_resize_buffer(&normals, sizeof(QOBJvec3), normalSize, &normalCap);
+			if(resizeError != QOBJ_SUCCESS)
+			{
+				errorCode = resizeError;
+				goto cleanup;
+			}
+		}
+		else if(strcmp(curToken, "vt") == 0)
+		{
+			QOBJvec2 texCoord;
+			fscanf(fptr, "%f %f\n", &texCoord.v[0], &texCoord.v[1]);
+
+			texCoords[texCoordSize++] = texCoord;
+			QOBJerror resizeError = qobj_maybe_resize_buffer(&texCoords, sizeof(QOBJvec2), texCoordSize, &texCoordCap);
+			if(resizeError != QOBJ_SUCCESS)
+			{
+				errorCode = resizeError;
+				goto cleanup;
+			}
+		}
+		else if(strcmp(curToken, "f") == 0)
+		{
+			QOBJuvec3 vertices[4];
+
+			int32_t i = 0;
+			while(true)
+			{
+				fscanf(fptr, "%d", &vertices[i].v[0]);
+				i++;
+
+				char nextCh = fgetc(fptr);
+				if(nextCh == ' ')
+					continue;
+				else if(nextCh == '\n')
+					break;
+				else
+				{
+					nextCh = fgetc(fptr);
+					if(nextCh == '/')
+						fscanf(fptr, "%d", &vertices[i].v[3]);
+					else
+						ungetc(nextCh, fptr);
+					
+					fscanf(fptr, "%d", &vertices[i].v[2]);
+					
+					nextCh = fgetc(fptr);
+					if(nextCh == '/')
+						fscanf(fptr, "%d", &vertices[i].v[3]);
+					else
+						ungetc(nextCh, fptr);
+				}				
+			}
+		}
+		else if(strcmp(curToken, "usemtl") == 0)
+		{
+			char materialName[128];
+			fgets(materialName, 128, fptr);
+
+			for(curMesh = 0; curMesh < *numMeshes; crMesh++)
+				if(strcmp((*meshes)[i].materialName, materialName) == 0)
+					break;
+
+			if(curMesh >= numMeshes)
+			{
+				(*numMeshes)++;
+				*meshes = realloc(*meshes, *numMeshes * sizeof(QOBJmesh));
+				if(!*meshes)
+				{
+					errorCode = QOBJ_ERROR_OUT_OF_MEM;
+					goto cleanup;
+				}
+
+				//TODO: SET MATERIAL
+			}
+		}
+		else if(strcmp(curToken, "mtllib") == 0)
+		{
+			//TODO
+		}
+		else
+		{
+			errorCode = QOBJ_ERROR_UNSUPPORTED_DATA_TYPE;
+			goto cleanup;
+		}
+
+		if(curTokenEnd == EOF)
+			break;
+	}
 
 	cleanup: ;
+
+	free(curToken);
+
+	if(errorCode != QOBJ_SUCCESS)
+	{
+		qobj_free(*numMeshes, *meshes);
+		*meshes = NULL;
+	}
 
 	free(positions);
 	free(normals);
@@ -93,12 +289,6 @@ static QOBJerror qobj_load(const char* path, size_t* numMeshes, QOBJmesh** meshe
 
 	fclose(fptr);
 	return errorCode;
-}
-
-static void qobj_free(QOBJmesh mesh)
-{
-	free(mesh.vertices);
-	free(mesh.indices);
 }
 
 #ifdef __cplusplus
